12d6b3bbd70ce9ef94b84bdb28023e415b573b39
Mahyar Behzadi <mahyarbhz@gmail.com>
2024-08-21 10:26:05 +0330

    "Removed comments from App.java and added Javadoc comments to CLI.java"

--- app/src/main/java/todoapp/CLI.java
+++ app/src/main/java/todoapp/CLI.java
@@ -1,8 +1,20 @@
 package todoapp;
 
+/**
+ * Provides a command-line interface for the TO-DO application using Singleton design-pattern.
+ * @see <a href="https://refactoring.guru/design-patterns/singleton/">Singleton pattern</a>
+ * 
+ * @author Mahyar Behzadi, mahyarbhz@gmail.com
+ */
+
 public class CLI {
     private static CLI instance;
 
+    /**
+     * Returns the single instance of the CLI class.
+     * 
+     * @return the single instance of the CLI class
+     */
     public static CLI getInstance() {
         if (instance == null) {
             instance = new CLI();
@@ -10,6 +22,11 @@ public class CLI {
         return instance;
     }
 
+    /**
+     * CLI basic introduction
+     * 
+     * @return void
+     */
     public void intro() {
         System.out.println("Welcome to the TO-DO CLI application!\nI hope it helps you out.");
     }
